4186c9f073e6ee1a92d520a75b5fa8e8a2fdd123
ReTTy982 <polekpacz@gmail.com>
2023-01-07 20:12:57 +0100

    123123

--- database_project/settings.py
+++ database_project/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions.models.Session '
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wypozyczalnia',
