fc87d5aabaaaaa18cd5072371bd6eba6ce0236dc
polekpacz <polek@pacz.pacz>
2023-01-23 18:55:31 +0000

    wip: data population

--- wypozyczalnia/admin.py
+++ wypozyczalnia/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+import traceback
+from django.db import connection
 from .models import *
 
 admin.site.register(Book)
@@ -6,3 +8,17 @@ admin.site.register(Publisher)
 admin.site.register(Author)
 
 # Register your models here.
+
+
+#if Book.objects.count() == 0:
+#    print("Populating db")
+#    try:
+#        with open("data/populate.sql") as file:
+#            create_query = file.read()
+#
+#        with connection.cursor() as cursor:
+#            cursor.execute(create_query)
+#    
+#    except:
+#        print(traceback.format_exc())
+#
