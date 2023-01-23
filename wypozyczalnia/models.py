6b9d56f73bf7443070fdb81fbb7d9520332967e0
ReTTy982 <polekpacz@gmail.com>
2023-01-23 17:12:10 +0100

    123

--- wypozyczalnia/models.py
+++ wypozyczalnia/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Publisher(models.Model):
+    id = models.BigAutoField(primary_key=True)
     publisher_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=30,unique=True)
     class Meta:
